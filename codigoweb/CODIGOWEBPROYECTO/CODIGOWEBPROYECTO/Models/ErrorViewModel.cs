d18772453d1c65baac52b5b1269a0d10a591154e
SerValCor <sbvc98@outlook.com>
2023-04-10 16:30:36 -0600

    Codigo

---
+++ codigoweb/CODIGOWEBPROYECTO/CODIGOWEBPROYECTO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CODIGOWEBPROYECTO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
